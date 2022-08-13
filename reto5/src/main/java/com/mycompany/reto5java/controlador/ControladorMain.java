2fb922f706101b7672359baac35007fd4645362e
Sebaenz <96449181+Sebaenz@users.noreply.github.com>
2022-08-13 18:44:48 -0500

    first commit

---
+++ reto5/src/main/java/com/mycompany/reto5java/controlador/ControladorMain.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.reto5java.controlador;
+
+import java.sql.*;
+import com.mycompany.reto5java.modelo.DAO.Conexion;
+import com.mycompany.reto5java.vista.frmPrincipal;
+
+
+public class ControladorMain {
+    public static void main(String[] args){
+        new frmPrincipal().setVisible(true);
+        
+}
+}
